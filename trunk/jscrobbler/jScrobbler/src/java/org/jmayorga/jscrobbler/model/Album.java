5aff4bde5afc4df5d094c43a0263ebd7b6f5f86e
jlmayorga <jlmayorga@6d2135ac-6a32-0410-a50d-8b69926bcfac>
2007-06-09 03:10:53 +0000

    Added top Albums functionality

---
+++ trunk/jscrobbler/jScrobbler/src/java/org/jmayorga/jscrobbler/model/Album.java
@@ -0,0 +1,65 @@
+package org.jmayorga.jscrobbler.model;
+
+import java.awt.Image;
+
+public class Album {
+
+	private String name;
+
+	private String mbid;
+
+	private String reach;
+
+	private String url;
+
+	private Image image;
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public String getMbid() {
+		return mbid;
+	}
+
+	public void setMbid(String mbid) {
+		this.mbid = mbid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getReach() {
+		return reach;
+	}
+
+	public void setReach(String reach) {
+		this.reach = reach;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		String eol = System.getProperty("line.separator");
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("Name: ").append(name).append(eol);
+		return buffer.toString();
+	}
+
+}
