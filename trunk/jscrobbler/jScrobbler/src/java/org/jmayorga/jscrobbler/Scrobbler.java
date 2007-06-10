26c60be7feb695be0553b3b45abc76d3944ba49b
jlmayorga <jlmayorga@6d2135ac-6a32-0410-a50d-8b69926bcfac>
2007-06-10 04:38:35 +0000

    Added top tags functionality

--- trunk/jscrobbler/jScrobbler/src/java/org/jmayorga/jscrobbler/Scrobbler.java
+++ trunk/jscrobbler/jScrobbler/src/java/org/jmayorga/jscrobbler/Scrobbler.java
@@ -8,16 +8,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 import org.apache.commons.digester.Digester;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jmayorga.jscrobbler.model.Album;
 import org.jmayorga.jscrobbler.model.Artist;
-import org.jmayorga.jscrobbler.model.MostKnownTracks;
-import org.jmayorga.jscrobbler.model.SimilarArtists;
-import org.jmayorga.jscrobbler.model.TopAlbums;
+import org.jmayorga.jscrobbler.model.Tag;
 import org.jmayorga.jscrobbler.model.Track;
 import org.xml.sax.SAXException;
 
@@ -31,18 +31,18 @@ public class Scrobbler {
 	 * @return Similar artists
 	 */
 	public Collection<Artist> getSimilarArtists(String artist) {
-		Collection<Artist> similarArtists = null;
-		log.debug("Starting");
+
+		log.debug("Getting similar artists for: [" + artist + "]");
 
 		Digester digester = new Digester();
-		SimilarArtists digestedResult = null;
+		Collection<Artist> digestedResult = new ArrayList<Artist>();
 		try {
 			URL url = new URL("http://ws.audioscrobbler.com/1.0/artist/"
 					+ artist + "/similar.xml");
 
 			digester.setValidating(false);
-			digester.addObjectCreate("similarartists", SimilarArtists.class);
-			digester.addSetProperties("similarartists");
+			digester.addObjectCreate("similarartists", digestedResult
+					.getClass());
 			digester.addObjectCreate("similarartists/artist", Artist.class);
 			digester
 					.addBeanPropertySetter("similarartists/artist/name", "name");
@@ -58,10 +58,10 @@ public class Scrobbler {
 			digester.addBeanPropertySetter("similarartists/artist/streamable",
 					"streamable");
 
-			digester.addSetNext("similarartists/artist", "addArtist");
+			digester.addSetNext("similarartists/artist", "add");
 
-			digestedResult = (SimilarArtists) digester.parse(url);
-			similarArtists = digestedResult.getRelatedArtists();
+			digestedResult = (Collection<Artist>) digester.parse(url);
+
 			log.debug("Result: " + digestedResult);
 
 		} catch (MalformedURLException e) {
@@ -73,7 +73,7 @@ public class Scrobbler {
 		} catch (SAXException e) {
 			log.error("SAXException, ", e);
 		}
-		return similarArtists;
+		return digestedResult;
 	}
 
 	/**
@@ -82,18 +82,19 @@ public class Scrobbler {
 	 * @param artist
 	 * @return
 	 */
-	public MostKnownTracks getMostKnownTracks(String artist) {
-		log.debug("Starting");
+	public Collection<Track> getMostKnownTracks(String artist) {
+		log.debug("Getting most known tracks of: [" + artist + "]");
 
 		Digester digester = new Digester();
-		MostKnownTracks digestedResult = null;
+		Collection<Track> digestedResult = new HashSet<Track>();
+
 		try {
 			URL url = new URL("http://ws.audioscrobbler.com/1.0/artist/"
 					+ artist + "/toptracks.xml");
 			digester.setValidating(false);
 
-			digester.addObjectCreate("mostknowntracks", MostKnownTracks.class);
-			digester.addSetProperties("mostknowntracks");
+			digester.addObjectCreate("mostknowntracks", digestedResult
+					.getClass());
 			digester.addObjectCreate("mostknowntracks/track", Track.class);
 			digester
 					.addBeanPropertySetter("mostknowntracks/track/name", "name");
@@ -102,9 +103,9 @@ public class Scrobbler {
 			digester.addBeanPropertySetter("mostknowntracks/track/reach",
 					"reach");
 			digester.addBeanPropertySetter("mostknowntracks/track/url", "url");
-			digester.addSetNext("mostknowntracks/track", "addTrack");
+			digester.addSetNext("mostknowntracks/track", "add");
 
-			digestedResult = (MostKnownTracks) digester.parse(url);
+			digestedResult = (Collection<Track>) digester.parse(url);
 			log.debug("Result: " + digestedResult);
 
 		} catch (MalformedURLException e) {
@@ -125,25 +126,24 @@ public class Scrobbler {
 	 * @param artist
 	 * @return
 	 */
-	public TopAlbums getTopAlbums(String artist) {
-
+	public Collection<Album> getTopAlbums(String artist) {
+		log.debug("Getting top albums of: [" + artist + "]");
 		Digester digester = new Digester();
-		TopAlbums digestedResult = null;
+		Collection<Album> digestedResult = new HashSet<Album>();
 		try {
 			URL url = new URL("http://ws.audioscrobbler.com/1.0/artist/"
 					+ artist + "/topalbums.xml");
 			digester.setValidating(false);
 
-			digester.addObjectCreate("topalbums", TopAlbums.class);
-			digester.addSetProperties("topalbums");
+			digester.addObjectCreate("topalbums", digestedResult.getClass());
 			digester.addObjectCreate("topalbums/album", Album.class);
 			digester.addBeanPropertySetter("topalbums/album/name", "name");
 			digester.addBeanPropertySetter("topalbums/album/mbid", "mbid");
 			digester.addBeanPropertySetter("topalbums/album/reach", "reach");
 			digester.addBeanPropertySetter("topalbums/album/url", "url");
-			digester.addSetNext("topalbums/album", "addAlbum");
+			digester.addSetNext("topalbums/album", "add");
 
-			digestedResult = (TopAlbums) digester.parse(url);
+			digestedResult = (Collection<Album>) digester.parse(url);
 			log.debug("Result: " + digestedResult);
 
 		} catch (MalformedURLException e) {
@@ -158,5 +158,45 @@ public class Scrobbler {
 
 		return digestedResult;
 
+	}
+
+	/**
+	 * Gets top tags of a given artist name
+	 *
+	 * @param artist
+	 * @return
+	 */
+	public Collection<Tag> getTopTags(String artist) {
+		log.debug("Getting top tags of: [" + artist + "]");
+		Collection<Tag> tags = new HashSet<Tag>();
+
+		Digester digester = new Digester();
+
+		try {
+			URL url = new URL("http://ws.audioscrobbler.com/1.0/artist/"
+					+ artist + "/toptags.xml");
+			digester.setValidating(false);
+
+			digester.addObjectCreate("toptags", tags.getClass());
+			digester.addObjectCreate("toptags/tag", Tag.class);
+			digester.addBeanPropertySetter("toptags/tag/name", "name");
+			digester.addBeanPropertySetter("toptags/tag/count", "count");
+			digester.addBeanPropertySetter("toptags/tag/url", "url");
+			digester.addSetNext("toptags/tag", "add");
+
+			tags = (Collection<Tag>) digester.parse(url);
+			log.debug("Result: " + tags);
+
+		} catch (MalformedURLException e) {
+			log.error("Malformed URL, ", e);
+		} catch (FileNotFoundException e) {
+			log.error("Artist with name: [" + artist + "] was not found");
+		} catch (IOException e) {
+			log.error("IOException, ", e);
+		} catch (SAXException e) {
+			log.error("SAXException, ", e);
+		}
+
+		return tags;
 	}
 }
