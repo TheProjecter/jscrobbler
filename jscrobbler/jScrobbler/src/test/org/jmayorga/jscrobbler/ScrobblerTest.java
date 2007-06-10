18be61068da63001e6547134d16423a872240f0a
jlmayorga <jlmayorga@6d2135ac-6a32-0410-a50d-8b69926bcfac>
2007-06-10 04:53:32 +0000

    Added top tags functionality

--- jscrobbler/jScrobbler/src/test/org/jmayorga/jscrobbler/ScrobblerTest.java
+++ jscrobbler/jScrobbler/src/test/org/jmayorga/jscrobbler/ScrobblerTest.java
@@ -4,9 +4,10 @@ import java.util.Collection;
 
 import junit.framework.TestCase;
 
+import org.jmayorga.jscrobbler.model.Album;
 import org.jmayorga.jscrobbler.model.Artist;
-import org.jmayorga.jscrobbler.model.MostKnownTracks;
-import org.jmayorga.jscrobbler.model.TopAlbums;
+import org.jmayorga.jscrobbler.model.Tag;
+import org.jmayorga.jscrobbler.model.Track;
 
 public class ScrobblerTest extends TestCase {
 	Scrobbler scrobbler;
@@ -19,17 +20,22 @@ public class ScrobblerTest extends TestCase {
 
 	public void testGetSimilarArtists() {
 		Collection<Artist> artists = scrobbler.getSimilarArtists("Sylver");
-		assertNotNull("Artists must not be null", artists);
+		assertNotNull("Result must not be null", artists);
 	}
 
 	public void testGetMostKnownTracks() {
-		MostKnownTracks knownTracks = scrobbler.getMostKnownTracks("Sylver");
-		assertNotNull("MostKnownTracks must not be null", knownTracks);
+		Collection<Track> knownTracks = scrobbler.getMostKnownTracks("Sylver");
+		assertNotNull("Result must not be null", knownTracks);
 	}
 
 	public void testGetTopAlbums() {
-		TopAlbums topAlbums = scrobbler.getTopAlbums("Sylver");
-		assertNotNull("TopAlbums must not be null", topAlbums);
+		Collection<Album> topAlbums = scrobbler.getTopAlbums("Sylver");
+		assertNotNull("Result must not be null", topAlbums);
+	}
+
+	public void testGetTopTags() {
+		Collection<Tag> tags = scrobbler.getTopTags("Sylver");
+		assertNotNull("Result must not be null", tags);
 	}
 
 }
